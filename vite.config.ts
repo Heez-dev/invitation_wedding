1766c78b0011456feb42cc3a8f7c2b0a9326051c
heez.dev <heez.dev@gmail.com>
2025-07-24 22:33:27 +0900

    chore: SCSS에서 경로 인식을 위한 @scss alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // SCSS에서는 vite-tsconfig-paths가 동작하지 않기 때문에 alias 별도 명시
+      '@scss': path.resolve(__dirname, 'src/scss'),
+      // 필요한 경우 다른 alias도 추가
+    },
+  },
 })
