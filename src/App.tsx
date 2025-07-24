087c2575f8d7d4e411e54dc326becd3830994b3d
heez.dev <heez.dev@gmail.com>
2025-07-24 22:34:10 +0900

    feat: Intro, Invitation 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Heading from '@/components/sections/Heading'
 import type { Wedding } from '@/models/wedding'
 import Video from '@/components/sections/Video'
 import ImageGallery from '@/components/sections/ImageGallery'
+import Intro from '@/components/sections/Intro'
+import Invitaion from '@/components/sections/Invitaion'
 
 const cx = classNames.bind(styles)
 
@@ -44,14 +46,28 @@ function App() {
 
   if (wedding === null) return null
 
-  const { date, galleryImages } = wedding
+  const {
+    date,
+    galleryImages,
+    groom,
+    bride,
+    location,
+    message: { intro, invitation },
+  } = wedding
 
   return (
     <div className={cx('container')}>
       <Heading date={date} />
       <Video />
+      <Intro
+        groomName={groom.name}
+        brideName={bride.name}
+        locationName={location.name}
+        date={date}
+        message={intro}
+      />
+      <Invitaion message={invitation} />
       <ImageGallery images={galleryImages} />
-      {JSON.stringify(wedding)}
     </div>
   )
 }
